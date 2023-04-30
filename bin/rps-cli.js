ad466553d8f94c9540a94ddaace21abd5e240ec6
Brynna Miller <brynnam@email.unc.edu>
2023-04-29 23:19:44 -0400

    added imports

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
+import {rps, help, rules} from "../lib/rpsls.js"
+import minimist from "minimist";
+const args = minimist(process.argv.slice(2),{
+    alias: {
+        r: "rules"
+    }
+});
 
+//add help + rules calling
+
+//play the game
+
+//edge case: make sure result defined
